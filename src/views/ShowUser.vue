5a87f96acf9ba78c8b6e726af54c1d72a2effa18
TrigrD3 <arya66151@gmail.com>
2023-06-17 11:49:59 +0800

    .

---
+++ src/views/ShowUser.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p class="text-muted mb-0">Nama</p>
+    <h6>{{ user.name }}</h6>
+
+    <p class="text-muted mb-0">Username</p>
+    <h6>{{ user.username }}</h6>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    getUser() {
+      axios.get(`/users/${this.id}`).then((response) => {
+        this.user = response.data;
+      });
+    },
+  },
+  mounted() {
+    this.getUser();
+  },
+};
+</script>
+
+<style></style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,27 +1,67 @@
 <template>
-  This is Users page
+  <h6 class="mb-3">Data User</h6>
 
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">No</th>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">Address</th>
-        <th scope="col">Company</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td colspan="6" class="text-center">No Data</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-responsive">
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th width="10">No</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Company</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody v-if="users.length">
+        <tr v-for="(user, index) in users" :key="user.id">
+          <td>{{ index + 1 }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.address.street }}</td>
+          <td>{{ user.company.name }}</td>
+          <td>
+            <router-link
+              :to="{
+                name: 'users.show',
+                params: {
+                  id: user.id,
+                },
+              }"
+              class="btn btn-primary btn-sm"
+              >Detail
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+      <tbody v-else>
+        <tr>
+          <td colspan="6" class="text-center">-no data-</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    getUsers() {
+      axios.get('/users').then((response) => {
+        this.users = response.data;
+      });
+    },
+  },
+  mounted() {
+    this.getUsers();
+  },
+};
 </script>
 
 <style></style>
